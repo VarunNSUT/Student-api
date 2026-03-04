10459717a0b73e5df7243087287d0e93cf5c6bf5
VarunNSUT <thakurvarun262@gmail.com>
2026-03-04 11:25:41 +0530

    updated on 4 march 2026

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/VarunNSUT/Students-api/internal/config"
+)
 
 func main() {
-	fmt.Println("this is my project")
+	//steps to set up the entry point 
+	// 1. load config 
+	// 2. database setup
+	// 3. setup router 
+	// 4. set up server 
+
+
+	//LOAD CONFIG
+	cfg := config.MustLoad()
+
+	// SETUP ROUTER 
+	// go has a powerful net http package to make server or router instead of a third party package 
+	router := http.NewServeMux()
+
+	//http.ResponseWriter is an interface provided by Go's net/http package that lets your server write the HTTP response.
+	router.HandleFunc("/" , func(w http.ResponseWriter , r *http.Request){
+		w.Write([]byte("welcome to students api"))
+	})
+
+	// SETUP SERVER
+	server := http.Server {
+		Addr: cfg.HTTPServer.Addr , 
+		Handler: router,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal("failed to start the server")
+	}
+
+	fmt.Println("server started")
 }

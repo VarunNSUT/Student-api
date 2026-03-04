3d20bdf25eee593b751eb816d0b73814d80c57e9
VarunNSUT <thakurvarun262@gmail.com>
2026-03-04 12:21:44 +0530

    modified 4 march 12:21 pm

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/VarunNSUT/Students-api/internal/config"
 )
@@ -34,10 +40,32 @@ func main() {
 		Handler: router,
 	}
 
-	err := server.ListenAndServe()
+	fmt.Println("server started")
+
+	done := make(chan os.Signal , 1) // we take signal as the channel input here from the os 
+
+	signal.Notify(done , os.Interrupt , syscall.SIGINT , syscall.SIGTERM)
+
+	go func (){ // this will run concurrently so when the execution arrives here then our code will end , so we resolve this issue by channels / synchronising
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal("failed to start the server")
+		}
+	}()
+	
+	// the statement below is blocking
+	<- done //due to this our channel is blocked and main program will run unless complete process is over 
+
+	slog.Info("shutting down the server") // slog is reocording what your program is doing 
+
+	// report the shutdown by context pacakge 
+	ctx , cancel :=context.WithTimeout(context.Background() , 5*time.Second) // pass an empty starting point that is the background function in this packae itsellf 
+	defer cancel() 
+	
+	err := server.Shutdown(ctx) // this method gracefully shuts down the server 
 	if err != nil {
-		log.Fatal("failed to start the server")
+		slog.Error("failed to shutdown the server" , slog.String("error" , err.Error()))
 	}
 
-	fmt.Println("server started")
+	slog.Info("server shutdown successfully")
 }
